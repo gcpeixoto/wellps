374146c905da0cc8f4e9d5bfad8fe7390bac1540
Gustavo Peixoto <tavolesliv@gmail.com>
2018-09-05 01:22:35 -0300

    Fixing DRT connections to produce outputs for different DRTs.

--- lib/findDRTConnections.m
+++ lib/findDRTConnections.m
@@ -1,4 +1,4 @@
-function C = findDRTConnections(drt, D, nofsc, tocsv, dv)
+function C = findDRTConnections(drt, P, ave, base, nofsc, tocsv, dv)
 %FINDDRTCONNECTIONS searches for all the connected components 
 %                   of cells with same DRT value
 %
@@ -7,8 +7,15 @@ function C = findDRTConnections(drt, D, nofsc, tocsv, dv)
 %   
 %       drt     - array listing the wanted DRT values
 %
-%       D       - the 3D DRT array where the DRT chosen values will be
-%                 sought
+%       P       - structure of properties ('PHI','KX',etc.)
+%
+%       ave     - which average to use to recover values. 
+%                 There are 3 options: 'arithmetic', 'geometric',
+%                 'normalized'. (see ::computeParams to see definitions)
+%
+%       base    - which log base to use to compute properties.
+%                 There are 2 options: 'log10', 'ln', where 'ln' is the
+%                 Napier log.
 %
 %       nofsc   - acronym for 'number of significant cells'. This is 
 %                 the minimum number of cells that will be considered 
@@ -30,8 +37,125 @@ if ~isnumeric(drt)
     error('wellps:findDRTConnections','drt must be an array containing DRT values to be gone through.');    
 end
 
-if ~isnumeric(D) || numel(size(D)) ~= 3
-    error('wellps:findDRTConnections','Check array. Not a 3D one.');    
+if ~isstruct(P)
+    error('wellps:findDRTConnections','Argument must be a structure.');    
+end
+
+% copying fundamental properties 
+PHI = P.PHI;    
+PHIZ = P.PHIZ;  
+KX = P.KX;  
+KY = P.KY;  
+KZ = P.KZ;
+
+if strcmp(base,'ln')   
+    
+    LOGPHIZ = P.LNPHIZ;
+    logphiz = 'lnphiz,'; % .csv column name
+    
+elseif strcmp(base,'log10')  
+    
+    LOGPHIZ = P.Log10PHIZ;
+    logphiz = 'log10phiz,'; % .csv column name
+    
+end
+
+% copying according to ave and log-base
+switch ave
+    
+    case 'arithmetic' 
+        
+        KMEAN = P.KA;
+        RQI = P.RQIA;
+        FZI = P.FZIA;    
+        
+        % .csv column names
+        kmean = 'ka,'; 
+        rqi = 'RQIA,';
+        fzi = 'FZIA,';
+        
+        if strcmp(base,'ln')    
+            
+            DRT = P.DRTA_LN;            
+            LOGRQI = P.LNRQIA;
+            
+            logrqi = 'lnRQIA';
+    
+        elseif strcmp(base,'log10') 
+    
+            DRT = P.DRTA_LOG10;            
+            LOGRQI = P.Log10RQIA;
+            
+            logrqi = 'log10RQIA';
+    
+        else
+        
+            error('wellps:findDRTConnections','Log base not available.');
+
+        end
+
+    case 'geometric' 
+        
+        KMEAN = P.KG;
+        RQI = P.RQIG;
+        FZI = P.FZIG;
+        
+        % .csv column names
+        kmean = 'kg,'; 
+        rqi = 'RQIG,';
+        fzi = 'FZIG,';
+        
+        if strcmp(base,'ln') 
+            
+            DRT = P.DRTG_LN;            
+            LOGRQI = P.LNRQIG;
+            
+            logrqi = 'lnRQIG';
+    
+        elseif strcmp(base,'log10') 
+    
+            DRT = P.DRTG_LOG10;            
+            LOGRQI = P.Log10RQIG;
+            
+            logrqi = 'log10RQIG';
+    
+        else
+        
+            error('wellps:findDRTConnections','Log base not available.');
+
+        end
+    
+    case'normalized' 
+        
+        KMEAN = P.KN;
+        RQI = P.RQIN;
+        FZI = P.FZIN;
+        
+        % .csv column names
+        kmean = 'kn,'; 
+        rqi = 'RQIN,';
+        fzi = 'FZIN,';
+        
+        if strcmp(base,'ln')     
+            
+            DRT = P.DRTN_LN;            
+            LOGRQI = P.LNRQIN;
+            
+            logrqi = 'lnRQIN';
+    
+        elseif strcmp(base,'log10') 
+    
+            DRT = P.DRTN_LOG10;            
+            LOGRQI = P.Log10RQIN;
+            
+            logrqi = 'log10RQIN';
+    
+        else
+        
+            error('wellps:findDRTConnections','Log base not available.');
+
+        end
+        
 end
 
 if ~all(drt)
@@ -48,7 +172,11 @@ end
 
 
 % file header used in the loop
-hdr = {'i,'; 'j,'; 'k'}'; % transposed!
+hdr = {'i,'; 'j,'; 'k,'; 'phi_e,'; ...
+        'kx,'; 'ky,'; 'kz,'; ...
+        kmean; ...
+        'phiz,'; rqi; fzi; ...
+        logphiz; logrqi}'; % transposed!
 hdr = sprintf('%s\t',hdr{:});
 hdr(end)='';
 
@@ -68,7 +196,7 @@ for val = drt(1):drt(end)
     fprintf('----> Sweeping field: DRT = %d... \n',val);
     
     % filtering grid to capture voxels with a specific DRT
-    [ ~, coordsDRT, indz ] = maskVoxels(D,val); 
+    [ ~, coordsDRT, indz ] = maskVoxels(DRT,val); 
     
     
     %{
@@ -159,23 +287,42 @@ for val = drt(1):drt(end)
     [ncomp,compSizes,members] = networkComponents(MDadj);
             
     % global  
-    drtSt.value = val;                     % DRT value
+    drtSt.value = val;                        % DRT value
     drtSt.allAdjMatrix = MDadj;               % graph adjacency matrix
     drtSt.allAdjEdgeList = edgeList;          % graph edge list
     drtSt.allVoxelCoords = coordsDRT;         % cell coordinates (i,j,k)     
-    drtSt.allVoxelInds = indz;                % cell linear indices    
+    drtSt.allVoxelInds = indz;                % cell linear indices
+    drtSt.allPHI = PHI( indz );               % effective porosity
+    drtSt.allKX = KX( indz );                 % permeability-x
+    drtSt.allKY = KY( indz );                 % permeability-y
+    drtSt.allKZ = KZ( indz );                 % permeability-z
+    drtSt.allKMEAN = KMEAN( indz );           % average permeability 
+    drtSt.allPHIZ = PHIZ( indz );             % normalized porosity
+    drtSt.allRQI = RQI( indz );               % RQI
+    drtSt.allFZI = FZI( indz );               % FZI
+    drtSt.allLogPHIZ = LOGPHIZ( indz );       % LogPHIZ
+    drtSt.allLogRQI = LOGRQI( indz );         % LogRQI
     drtSt.allNComps = ncomp;                  % number of components in the graph
 
     % mount matrix to export
     mat = [ coordsDRT(:,1) ... 
             coordsDRT(:,2) ...
             coordsDRT(:,3) ...
-            ];
+            PHI( indz )    ...
+            KX( indz )     ...
+            KY( indz )     ...
+            KZ( indz )     ...
+            KMEAN( indz )  ...
+            PHIZ( indz )   ...
+            RQI( indz )    ...
+            FZI( indz )    ...
+        LOGPHIZ( indz )    ...
+        LOGRQI( indz )   ];            
             
     if tocsv == true
         
         % preparing csv file
-        fname = strcat('../csv/Table-Field','_DRT_',num2str( val ),'.csv');    
+        fname = strcat('../csv/Table-Field_DRT_',ave,'_',base,'_',num2str( val ),'.csv');    
         dlmwrite(fname,hdr,'');        
     
         % append matrix
@@ -191,17 +338,38 @@ for val = drt(1):drt(end)
         
         aux = coordsDRT( members{idcomp},: );
         drtSt.compVoxelCoords{idcomp} = aux; 
-        drtSt.compVoxelInds{idcomp} = indz( members{idcomp} );        
-        drtSt.compNNodes{idcomp} = compSizes(idcomp);                
+        drtSt.compVoxelInds{idcomp} = indz( members{idcomp} );          
+        drtSt.compNNodes{idcomp} = compSizes(idcomp);           
+        drtSt.compPHI{idcomp} = PHI( indz( members{idcomp} ) );
+        drtSt.compKX{idcomp} = KX( indz( members{idcomp} ) );
+        drtSt.compKY{idcomp} = KY( indz( members{idcomp} ) );
+        drtSt.compKZ{idcomp} = KZ( indz( members{idcomp} ) );
+        drtSt.compKMEAN{idcomp} = KMEAN( indz( members{idcomp} ) );
+        drtSt.compPHIZ{idcomp} = PHIZ( indz( members{idcomp} ) );
+        drtSt.compRQI{idcomp} = RQI( indz( members{idcomp} ) );
+        drtSt.compFZI{idcomp} = FZI( indz( members{idcomp} ) );
+        drtSt.compLogPHIZ{idcomp} = LOGPHIZ( indz( members{idcomp} ) );
+        drtSt.compLogRQI{idcomp} = LOGRQI( indz( members{idcomp} ) ); 
                   
         % matrix to export
-        mat = [ aux(:,1) ... 
-                aux(:,2) ...
-                aux(:,3) ];
+        mat = [ aux(:,1)                         ... 
+                aux(:,2)                         ...
+                aux(:,3)                         ...
+                PHI( indz( members{idcomp} ) )   ...
+                 KX( indz( members{idcomp} ) )   ...
+                 KY( indz( members{idcomp} ) )   ...
+                 KZ( indz( members{idcomp} ) )   ...
+              KMEAN( indz( members{idcomp} ) )   ...
+               PHIZ( indz( members{idcomp} ) )   ...
+                RQI( indz( members{idcomp} ) )   ...
+                FZI( indz( members{idcomp} ) )   ...
+            LOGPHIZ( indz( members{idcomp} ) )   ...
+            LOGRQI( indz( members{idcomp} ) )  ];
+           
         
         if (tocsv == true) && (compSizes(idcomp) >= nofsc)              
             % preparing csv file
-            fname = strcat('../csv/Table-Cluster_',num2str(idcomp),'_DRT_',num2str( val ),'.csv');        
+            fname = strcat('../csv/Table-Cluster_',num2str(idcomp),'_DRT_',ave,'_',base,'_',num2str( val ),'.csv');        
             dlmwrite(fname,hdr,'');       
             
             % append matrix 
@@ -212,7 +380,7 @@ for val = drt(1):drt(end)
     end              
 
     % saving structure to .mat     
-    save( strcat('../mat/DRT_',num2str( val ),'.mat'),'drtSt'); % saving
+    save( strcat('../mat/DRT_',ave,'_',base,'_',num2str( val ),'.mat'),'drtSt'); % saving
     fprintf('----> DRT_%s.mat file saved. \n',num2str(val));
     
     % dynamic attribution     
