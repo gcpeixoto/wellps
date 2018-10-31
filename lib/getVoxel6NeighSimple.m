771cf119c8334722b9ef5731760f0b3fa6850039
Gustavo Peixoto <tavolesliv@gmail.com>
2018-10-30 22:13:50 -0300

    Fixing error in assert condition.

--- lib/getVoxel6NeighSimple.m
+++ lib/getVoxel6NeighSimple.m
@@ -7,7 +7,7 @@ function vc6n = getVoxel6NeighSimple(vc)
 %   output:
 %        vc6n: 6-neighbour voxel coordinates
 
-assert(size(vc) ~= [1,3],'seed voxel must be a 1x3 array.');
+assert(all(size(vc) == [1,3]),'seed voxel must be a 1x3 array.');
 
 % 6-neighbour array [+x,-x,+y,-y,+z,-z]'
 vc6n = repmat(vc,[6,1]) + [1,0,0; -1,0,0; 0,1,0; 0,-1,0; 0,0,1; 0,0,-1];
