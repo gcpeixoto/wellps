aa558493cfa2dac9cb84d88ad300455669e210ad
Gustavo Peixoto <tavolesliv@gmail.com>
2020-02-26 11:32:50 -0300

    Minors

--- lib/buildModelSPE10.m
+++ lib/buildModelSPE10.m
@@ -14,26 +14,33 @@ function [G,PROPS] = buildModelSPE10(orig)
 %        (porosity, permeability x,y,z, etc.)
 %
 
+d = DirManager;
+bmark = d.getBenchMarksDir;
+
+f = fullfile(bmark,'spe10');
+
 
 % load original G structure
-load('../benchmarks/spe10/mrst/G-SPE10.mat','G');
+load(fullfile(f,'mrst/G-SPE10.mat'),'G');
 
 % load rock structure 
 
-switch orig
+switch orig % original data is in [m^2]
+      
     case 'mrst'
-% original data is in [m^2]
-        load('../benchmarks/spe10/mrst/rock-SPE10.mat');
+        
+        load(fullfile(f,'mrst/rock-SPE10.mat'));        
         PROPS.PHI = reshape(rock.poro,G.cartDims);
         PROPS.KX = reshape(rock.perm(:,1),G.cartDims);
         PROPS.KY = reshape(rock.perm(:,2),G.cartDims);
         PROPS.KZ = reshape(rock.perm(:,3),G.cartDims);
         
     case 'original'
-        load('../benchmarks/spe10/original/PHI.mat');
-        load('../benchmarks/spe10/original/KX.mat');
-        load('../benchmarks/spe10/original/KY.mat');
-        load('../benchmarks/spe10/original/KZ.mat');
+        
+        load(fullfile(f,'original/PHI.mat'));        
+        load(fullfile(f,'original/KX.mat'));        
+        load(fullfile(f,'original/KY.mat'));        
+        load(fullfile(f,'original/KZ.mat'));                
         PROPS.PHI = PHI;
         PROPS.KX = KX;
         PROPS.KY = KY;
