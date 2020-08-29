b7733563e9143b8fc9785235630e4640e6922ce3
Gustavo Peixoto <tavolesliv@gmail.com>
2020-08-29 11:34:15 -0300

    Including benchmark case psy and updating produnits case study on top of psy.

--- cases/case_unisim1_prodUnits.m
+++ cases/case_unisim1_prodUnits.m
@@ -21,19 +21,19 @@
 %                'NTG'   , 'VSH'   , **'SO'**    ...
 %                }
 %
-% Moreover, for this case, we used a duplicata file 'UNISIM_....SO.DATA'
-% which has a INCLUDE call to the file 'SO.INC' for the distribution 
+% Moreover, for this case, we used a the file 'PSY.DATA'
+% which has an INCLUDE call to the file 'SO.INC' for the distribution 
 % of oil saturation obtained by the standard relative permeability curves 
 % of the model. 
 
 
-case_name = 'unisim1-prodUnits';  
+case_name = 'psy-prodUnits';  
 
 %% Loading grid 
 
 d = DirManager(); 
 
-f = fullfile(d.getBenchMarksDir,'unisim-I-D','eclipse','UNISIM_I_D_ECLIPSE_SO.DATA');
+f = fullfile(d.getBenchMarksDir,'psy','eclipse','PSY.DATA');
 
 % Unable to call buildModel because SO is a nonstandard field
 G = readGRDECL(f);
@@ -44,6 +44,7 @@ PROPS.KZ  = G.PERMZ;
 PROPS.ACTNUM  = G.ACTNUM;
 PROPS.SO  = G.SO;
 G = processGRDECL(G);
+G = computeGeometry(G);
 
 % active cells
 on = find(PROPS.ACTNUM == true);
@@ -80,6 +81,7 @@ RQI_SO_norm = RQI_SO_norm(in);
 % plot proxy
 %Gn = extractSubgrid(G,in);
 %plotCellData(Gn,RQI_SO_norm,'FaceAlpha',1.0,'EdgeColor','none')
+%axis vis3d off
 %colormap jet
 
 % plot RQI_norm > 0
@@ -146,74 +148,170 @@ nofsc = 10; % only for .csv
 puc = 1:3; % only 3 classes matter
 pucSt = findConnectionsByPUC(d,puc,PUC,nofsc,'y',1);
 
-% PU clusters of class 3
-nofc = 10; % minimum number of cells to consider to get clusters
-sc1 = find(cell2mat(pucSt.PUC1.compNNodes) > nofc);
-sc2 = find(cell2mat(pucSt.PUC2.compNNodes) > nofc);
-sc3 = find(cell2mat(pucSt.PUC3.compNNodes) > nofc);
 
-%% Plots
+% minimum number of cells to consider to get clusters
+nofc = 10; 
 
-nc1 = length(sc1);
-nc2 = length(sc2);
-nc3 = length(sc3);
-cells_sc1 = cell(1,nc1);
-cells_sc2 = cell(1,nc2);
-cells_sc3 = cell(1,nc3);
+pucx = fieldnames(pucSt);
+nn = length(pucx);
 
-% plot proxy
-subplot(2,2,1)
+%% Get info for PU clusters 
+% The struct PUInfo stores information of clusters per PUC.
+
+for f = 1:nn
+    
+    % id of clusters per PUC with >= nofc elements
+    idsel = find ( cellfun( @numel, pucSt.(pucx{f}).compVoxelInds) >= nofc );
+    
+    if ~isempty(idsel)
+        
+        for C = idsel
+        
+            PUInfo.(pucx{f}).(strcat('C',num2str(C))).value = ...
+            pucSt.(pucx{f}).value;
+                                    
+            PUInfo.(pucx{f}).(strcat('C',num2str(C))).compVoxelInds = ...
+            pucSt.(pucx{f}).compVoxelInds{C};
+        
+            PUInfo.(pucx{f}).(strcat('C',num2str(C))).mappedCompVoxelInds = ...
+            Ind( pucSt.(pucx{f}).compVoxelInds{C} ); % mapped inds
+            
+            PUInfo.(pucx{f}).(strcat('C',num2str(C))).compVoxelCoords = ...
+            pucSt.(pucx{f}).compVoxelCoords{C};                                    
+                                    
+        end
+    end
+end
+
+%% Computation of closeness centrality 
+% In this section, we compute the closeness centrality for each 
+% productivity unit (cluster) and export tailored files to setup 
+% IMEX perforation tables.
+
+for f = 1:nn % loop PUC
+    
+    fn = fieldnames(PUInfo.(pucx{f}));
+    
+    for fc = 1:length(fn) % loop clusters
+                            
+        % metrics 
+        cvi = PUInfo.(pucx{f}).(fn{fc}).compVoxelInds;            
+        cvc = PUInfo.(pucx{f}).(fn{fc}).compVoxelCoords;
+                                
+        indIJ = [];
+        for i = 1:numel(cvi)
+            for j = 1:numel(cvi)
+                  if i ~= j % skipping null distance                   
+                      dist = sqrt( ( cvc(i,1) - cvc(j,1) )^2 + ...
+                                   ( cvc(i,2) - cvc(j,2) )^2 + ...
+                                   ( cvc(i,3) - cvc(j,3) )^2 ); 
+
+                      % detecting neighbour voxels                  
+                      if dist <= 1     % connectivity criterion
+                          indIJ = [ indIJ; [ i j ] ];
+                          edgeList = indIJ;                      
+                      end                  
+                  end
+             end
+        end   
+
+        % invoke graphmetrics
+        aux = [ indIJ(:,2) indIJ(:,1) ]; % reverse edges [ j i ]
+        indIJ = [ indIJ; aux ]; % filling       
+
+        % !!! This 0.5 was placed here because with 1, the sparse 
+        % matrix was returning entries = 2. Why is that??
+        Madj = sparse( indIJ(:,1),indIJ(:,2),0.5,size(cvc,1),size(cvc,1) ); 
+
+        edfile = saveAdjEdges(Madj);              
+        outfile = fullfile(d.getTmpDir,'metrics.txt');
+        exec = sprintf('graphMetrics %s %s',edfile,outfile);
+
+        % call SNAP 
+        fprintf('%s\n',repmat('=',[1,75]))
+        fprintf('%sWELLPS - SNAP Interface :: running C++\n',repmat(' ',[1,20]));
+        fprintf('%s\n',repmat('=',[1,75]))
+        system( fullfile(d.getCppDir,exec) );
+
+        tab = importdata(outfile);                       
+        Mtab = tab.data(:,1:4);            
+        nodeID = Mtab(:,1);
+
+        % centralities            
+        cln  = Mtab(:,3);            
+
+        % delete the temporary files
+        delete(outfile)                                    
+
+        % get rid of input file
+        delete(edfile);
+
+        maxC = max(cln);        % max closeness = min farness
+        iC = cln == maxC;       % network closer nodes
+        iCnode = nodeID(iC);    % getting node id (there might be more than 1)
+        ivC = cvc(iCnode,: );   
+
+        % save info
+        PUMetrics.(pucx{f}).(fn{fc}).maxClosenessVoxelCoords = ivC(1,:);
+        PUMetrics.(pucx{f}).(fn{fc}).maxClosenessValue = maxC;
+
+        aux = cvi(iCnode); aux = aux(1); % gets only first of the list
+        PUMetrics.(pucx{f}).(fn{fc}).maxClosenessVoxelInd = aux;
+
+        aux = Ind(cvi(iCnode)); aux = aux(1); % gets only first of the list
+        PUMetrics.(pucx{f}).(fn{fc}).maxClosenessVoxelMappedInd = aux;                
+
+        %save perforation table for cluster MaxC       
+        ptset.savedir = fullfile(d.getTmpDir,'PSY','MaxC',pucx{f},fn{fc});
+        ptset.wellname = 'W';
+        ptset.geometry = 'K';
+        ptset.perfs = PUMetrics.(pucx{f}).(fn{fc}).maxClosenessVoxelCoords;
+        ptset = savePerfTable(ptset);
+        
+        clear ptset
+        
+    end
+
+end
+
+
+%% Plot proxy
+figure
 Gn = extractSubgrid(G,in);
 plotCellData(Gn,RQI_SO_norm,'FaceAlpha',1.0,'EdgeColor','none')
-axis off
-colormap jet
-title('RQI_n x so')
+axis off; colormap jet; colorbar
+title('$RQI_n \times s_o(t)$','interpreter','latex')
 
-% PUC 1
-subplot(2,2,2)
-colormap jet
-plotGrid(G,'FaceColor','k','FaceAlpha',0.05,'EdgeColor','none')
-axis off
-colorbar
-title('PUC 1')
-for i = 1:nc1
-    cid = sc1(i);
-    glob = pucSt.PUC1.compVoxelInds{cid}; % global indices
-    cvim = Ind(glob); % mapped indices            
-    cells_sc1{i} = cvim;
-    Gg = extractSubgrid(G,cvim); % get correct indices
-    plotCellData(Gg,RQIso(glob),'EdgeColor','k'); % proxy values in relaation to global
+%% Plot Clusters 
+figure
+plotGrid(G,'FaceAlpha',0.1,'FaceColor','k','EdgeColor','None'); 
+colormap(jet); axis off; colorbar
+
+% mask
+mask = false(G.cells.num,1);
+
+% example values taken from PUInfo
+exi = 1; % exi-th PUC value in the list
+
+% exic-th cluster in the PUC list
+for exic = 1:length(fieldnames(PUInfo.(pucx{exi}))) 
+    
+    exPUC = fieldnames(PUInfo); 
+    exPUC = exPUC{exi};
+    exC = fieldnames(PUInfo.(exPUC)); 
+    exC = exC{exic};
+    excvi = PUInfo.(exPUC).(exC).compVoxelInds; % global
+    excvim = PUInfo.(exPUC).(exC).mappedCompVoxelInds; % local
+    exRQIso = RQIso(excvi); % proxy computed at the global index
+
+    % background + cluster domain
+    plotCellData(extractSubgrid(G,excvim),exRQIso)
+    
+    title(strcat('Productivity Units - PUC ',num2str(exi)))
+    % max clo (cluster)
+    indmax = PUMetrics.(exPUC).(exC).maxClosenessVoxelMappedInd;
+    mask(indmax) = true;
+    plotGrid(G,mask,'FaceColor','k','EdgeColor','w' );
 end
 
 
-% PUC 2
-subplot(2,2,3)
-colormap jet
-plotGrid(G,'FaceColor','k','FaceAlpha',0.05,'EdgeColor','none')
-axis off
-colorbar
-title('PUC 2')
-for i = 1:nc2
-    cid = sc2(i);
-    glob = pucSt.PUC2.compVoxelInds{cid}; % global indices
-    cvim = Ind(glob); % mapped indices            
-    cells_sc2{i} = cvim;
-    Gg = extractSubgrid(G,cvim); % get correct indices
-    plotCellData(Gg,RQIso(glob),'EdgeColor','k'); % proxy values in relaation to global
-end
-
-% PUC 3
-subplot(2,2,4)
-colormap jet
-plotGrid(G,'FaceColor','k','FaceAlpha',0.05,'EdgeColor','none')
-axis off 
-colorbar
-title('PUC 3')
-for i = 1:nc3
-    cid = sc3(i);
-    glob = pucSt.PUC3.compVoxelInds{cid}; % global indices
-    cvim = Ind(glob); % mapped indices            
-    cells_sc3{i} = cvim;
-    Gg = extractSubgrid(G,cvim); % get correct indices
-    plotCellData(Gg,RQIso(glob),'EdgeColor','k'); % proxy values in relaation to global
-end
